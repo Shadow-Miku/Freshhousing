2ce8fe8c5b7a51c5062f9076129629b852869e76
Shadow-Nino <108553512+Shadow-Miku@users.noreply.github.com>
2023-01-22 14:46:41 -0600

    Creación de la plantilla
    Plantilla para manejar el header y footer del proyecto

--- bienesraices/resources/views/contacto.blade.php
+++ bienesraices/resources/views/contacto.blade.php
@@ -1,37 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices Contacto</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    
-    <header class="header">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img src="/img/logo.svg" alt="Logotipo de Bienes Raices">
-                </a>
-
-                <div class="mobile-menu">
-                    <img src="/img/barras.svg" alt="icono menu responsive">
-                </div>
-
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="/img/dark-mode.svg">
-                    <nav class="navegacion">
-                        <a href=" {{ route('nosotros') }} ">Nosotros</a>
-                        <a href=" {{ route('anuncios') }} ">Anuncios</a>
-                        <a href=" {{ route('blog') }} ">Blog</a>
-                        <a href=" {{ route('contacto') }} ">Contacto</a>
-                    </nav>
-                </div>
-                
-            </div> <!--.barra-->
-        </div>
-    </header>
+@extends('plantilla')
+<?php $inicio = false ?>
+@section('contenido')
 
     <main class="contenedor seccion">
         <h1>Contacto</h1>
@@ -102,21 +71,4 @@
             <input type="submit" value="Enviar" class="boton-verde">
         </form>
     </main>
-
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href=" {{ route('nosotros') }} ">Nosotros</a>
-                <a href=" {{ route('anuncios') }} ">Anuncios</a>
-                <a href=" {{ route('blog') }} ">Blog</a>
-                <a href=" {{ route('contacto') }} ">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Tecnologías Frescas Todos los derechos Reservados 2023 &copy;</p>
-    </footer>
-
-    <!--js del darkmode-->
-    <script src="/js/bundle.min.js"></script>
-</body>
-</html>
+@stop
